b5caf58d55959659b7ee6258ea1be20ed004cb67
NickGuo <guohao4@lenovo.com>
2017-05-10 16:59:19 +0800

    Refactor

--- app/src/main/java/dev/tornaco/tasker/app/service/TaskerBridgeService.java
+++ app/src/main/java/dev/tornaco/tasker/app/service/TaskerBridgeService.java
@@ -8,14 +8,8 @@ import android.support.annotation.Nullable;
 
 import org.newstand.logger.Logger;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
-
-import dev.tornaco.tasker.app.provider.SettingStore;
-import dev.tornaco.tasker.app.provider.Settings;
 import dev.tornaco.tasker.service.ITask;
-import dev.tornaco.tasker.service.ITaskListener;
+import dev.tornaco.tasker.service.ITaskExecutor;
 import dev.tornaco.tasker.service.ITaskerBridgeService;
 
 /**
@@ -26,18 +20,11 @@ import dev.tornaco.tasker.service.ITaskerBridgeService;
 
 public class TaskerBridgeService extends Service implements ITaskerBridgeService {
 
-    private List<ITask> tasks = new ArrayList<>();
+    private boolean taskDone;
 
     @Override
     public void onCreate() {
         super.onCreate();
-        Settings settings = new SettingStore("TaskerBridgeService");
-
-        for (int i = 0; i < 10; i++) {
-            ITask t = new ITask("Task@" + i, UUID.randomUUID().toString(), "Body@" + i);
-            tasks.add(t);
-            Logger.d("Adding task %s", t);
-        }
     }
 
     @Nullable
@@ -46,51 +33,35 @@ public class TaskerBridgeService extends Service implements ITaskerBridgeService
         return asBinder();
     }
 
-    @Override
-    public String version() throws RemoteException {
-        return "1.0.0-ALPHA";
-    }
-
-    @Override
-    public ITask nextTask() throws RemoteException {
-        return tasks.remove(0);
-    }
-
-    @Override
-    public boolean hasNextTask() throws RemoteException {
-        return tasks.size() != 0;
-    }
-
-    @Override
-    public void setTaskListener(ITaskListener listener) throws RemoteException {
-
-    }
 
     @Override
     public IBinder asBinder() {
         return new Bridge();
     }
 
+    @Override
+    public void onExecutorCreate(ITaskExecutor executor) throws RemoteException {
+        Logger.d("onExecutorCreate: %s", executor.getSerial());
+        String res = executor.execute(new ITask("pressHome", "ID", "Data"));
+        Logger.d("pressHome: %s", res);
+        taskDone = true;
+    }
+
+    @Override
+    public boolean shouldTerminate(ITaskExecutor executor) throws RemoteException {
+        return true;
+    }
+
     private class Bridge extends Stub {
 
         @Override
-        public String version() throws RemoteException {
-            return TaskerBridgeService.this.version();
+        public void onExecutorCreate(ITaskExecutor executor) throws RemoteException {
+            TaskerBridgeService.this.onExecutorCreate(executor);
         }
 
         @Override
-        public ITask nextTask() throws RemoteException {
-            return TaskerBridgeService.this.nextTask();
-        }
-
-        @Override
-        public boolean hasNextTask() throws RemoteException {
-            return TaskerBridgeService.this.hasNextTask();
-        }
-
-        @Override
-        public void setTaskListener(ITaskListener listener) throws RemoteException {
-
+        public boolean shouldTerminate(ITaskExecutor executor) throws RemoteException {
+            return TaskerBridgeService.this.shouldTerminate(executor);
         }
     }
 }
--- app/src/main/java/dev/tornaco/tasker/app/service/TaskerBridgeServiceProxy.java
+++ app/src/main/java/dev/tornaco/tasker/app/service/TaskerBridgeServiceProxy.java
@@ -3,16 +3,7 @@ package dev.tornaco.tasker.app.service;
 import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
-import android.os.RemoteException;
-import android.support.annotation.Nullable;
-import android.support.annotation.WorkerThread;
 
-import java.util.concurrent.CountDownLatch;
-
-import dev.tornaco.tasker.app.common.Consumer;
-import dev.tornaco.tasker.app.common.Holder;
-import dev.tornaco.tasker.service.ITask;
-import dev.tornaco.tasker.service.ITaskListener;
 import dev.tornaco.tasker.service.ITaskerBridgeService;
 
 /**
@@ -41,106 +32,10 @@ public class TaskerBridgeServiceProxy extends ServiceProxy {
         context.startService(i);
     }
 
-    @WorkerThread
-    public static String version(Context context) {
-        try {
-            final Holder<String> stringHolder = new Holder<>();
-            final CountDownLatch latch = new CountDownLatch(1);
-            create(context).version(new Consumer<String>() {
-                @Override
-                public void accept(@Nullable String s) {
-                    stringHolder.setData(s);
-                    latch.countDown();
-                }
-            });
-            latch.await();
-            return stringHolder.getData();
-        } catch (RemoteException ignored) {
-            return null;
-        } catch (InterruptedException e) {
-            return null;
-        }
-    }
-
-    @WorkerThread
-    public void version(final Consumer<String> consumer) throws RemoteException {
-
-        setTask(new ProxyTask() {
-            @Override
-            public void run() throws RemoteException {
-                String version = mService.version();
-                consumer.accept(version);
-            }
-        });
-    }
-
 
     @Override
     public void onConnected(IBinder binder) {
         mService = ITaskerBridgeService.Stub.asInterface(binder);
     }
 
-    public static void nextTask(Context context, Consumer<ITask> taskConsumer) {
-        try {
-            create(context).nextTask(taskConsumer);
-        } catch (RemoteException e) {
-            taskConsumer.accept(null);
-        }
-    }
-
-    public void nextTask(final Consumer<ITask> taskConsumer) throws RemoteException {
-        setTask(new ProxyTask() {
-            @Override
-            public void run() throws RemoteException {
-                ITask next = mService.nextTask();
-                taskConsumer.accept(next);
-            }
-        });
-    }
-
-    public static boolean hasNext(Context context) {
-        try {
-            final Holder<Boolean> holder = new Holder<>();
-            final CountDownLatch latch = new CountDownLatch(1);
-            create(context).hasNextTask(new Consumer<Boolean>() {
-                @Override
-                public void accept(@Nullable Boolean aBoolean) {
-                    holder.setData(aBoolean);
-                    latch.countDown();
-                }
-            });
-            latch.await();
-            return holder.getData();
-        } catch (RemoteException e) {
-            return false;
-        } catch (InterruptedException e) {
-            return false;
-        }
-    }
-
-    public void hasNextTask(final Consumer<Boolean> booleanConsumer) throws RemoteException {
-        setTask(new ProxyTask() {
-            @Override
-            public void run() throws RemoteException {
-                booleanConsumer.accept(mService.hasNextTask());
-            }
-        });
-    }
-
-    public static void setTaskListener(Context context, ITaskListener listener) {
-        try {
-            create(context).setTaskListener(listener);
-        } catch (RemoteException ignored) {
-
-        }
-    }
-
-    public void setTaskListener(final ITaskListener listener) throws RemoteException {
-        setTask(new ProxyTask() {
-            @Override
-            public void run() throws RemoteException {
-                mService.setTaskListener(listener);
-            }
-        });
-    }
 }
